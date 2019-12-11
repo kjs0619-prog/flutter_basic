18a51d9fdf67bc5786d0e227c5f278f1007c0f8a
Junsuk Oh <a811219@gmail.com>
2019-12-11 21:35:41 +0900

    할 일 완료 토글

--- chapter09/lib/main.dart
+++ chapter09/lib/main.dart
@@ -93,7 +93,7 @@ class _TodoListPageState extends State<TodoListPage> {
   Widget _buildWidget(DocumentSnapshot doc) {
     final todo = Todo(doc['title'], isDone: doc['isDone']);
     return ListTile(
-      onTap: () => _toggleTodo(todo),
+      onTap: () => _toggleTodo(doc),
       title: Text(
         todo.title,
         style: todo.isDone
@@ -126,9 +126,9 @@ class _TodoListPageState extends State<TodoListPage> {
   }
 
   // 할 일 완료/미완료 메서드
-  void _toggleTodo(Todo todo) {
-    setState(() {
-      todo.isDone = !todo.isDone;
+  void _toggleTodo(DocumentSnapshot doc) {
+    Firestore.instance.collection('todo').document(doc.documentID).updateData({
+      'isDone': !doc['isDone'],
     });
   }
 }
