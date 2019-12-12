83ba2684477f1f75895e57b31aa9ade6bf76fe37
Junsuk Oh <a811219@gmail.com>
2019-12-12 20:55:50 +0900

    챕터 10 완성 코드

---
+++ chapter10_end/lib/comment_page.dart
@@ -0,0 +1,43 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:flutter/material.dart';
+
+class CommentPage extends StatelessWidget {
+  final DocumentSnapshot document;
+
+  CommentPage(this.document);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('댓글'),
+      ),
+      body: StreamBuilder<QuerySnapshot>(
+        stream: _commentStream(),
+        builder: (context, snapshot) {
+          if (!snapshot.hasData) {
+            return Center(
+              child: CircularProgressIndicator(),
+            );
+          }
+          return ListView(
+            children: snapshot.data.documents.map((doc) {
+              return ListTile(
+                leading: Text(doc['writer']),
+                title: Text(doc['comment']),
+              );
+            }).toList(),
+          );
+        },
+      ),
+    );
+  }
+
+  Stream<QuerySnapshot> _commentStream() {
+    return Firestore.instance
+        .collection('post')
+        .document(document.documentID)
+        .collection('comment')
+        .snapshots();
+  }
+}
---
+++ chapter10_end/lib/detail_post_page.dart
@@ -0,0 +1,138 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/material.dart';
+
+class DetailPostPage extends StatelessWidget {
+  final DocumentSnapshot document;
+  final FirebaseUser user;
+
+  DetailPostPage({Key key, this.document, this.user}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('둘러보기'),
+      ),
+      body: _buildBody(),
+    );
+  }
+
+  Widget _buildBody() {
+    return SingleChildScrollView(
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: <Widget>[
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Row(
+              children: <Widget>[
+                CircleAvatar(
+                  backgroundImage: NetworkImage(document['userPhotoUrl']),
+                ),
+                Padding(
+                  padding: const EdgeInsets.only(left: 8.0),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: <Widget>[
+                      Row(
+                        children: <Widget>[
+                          Text(
+                            document['email'],
+                            style: TextStyle(fontWeight: FontWeight.bold),
+                          ),
+                          SizedBox(
+                            width: 8,
+                          ),
+                          StreamBuilder<DocumentSnapshot>(
+                            stream: _followingStream(),
+                            builder: (context, snapshot) {
+                              if (!snapshot.hasData) {
+                                return Text('로딩중');
+                              }
+
+                              var data = snapshot.data.data;
+                              if (data == null ||
+                                  data[document['email']] == null ||
+                                  data[document['email']] == false) {
+                                return GestureDetector(
+                                  onTap: _follow,
+                                  child: Text(
+                                    "팔로우",
+                                    style: TextStyle(
+                                        color: Colors.blue,
+                                        fontWeight: FontWeight.bold),
+                                  ),
+                                );
+                              }
+
+                              return GestureDetector(
+                                onTap: _unfollow,
+                                child: Text(
+                                  "언팔로우",
+                                  style: TextStyle(
+                                      color: Colors.red,
+                                      fontWeight: FontWeight.bold),
+                                ),
+                              );
+                            },
+                          ),
+                        ],
+                      ),
+                      Text(document['displayName']),
+                    ],
+                  ),
+                )
+              ],
+            ),
+          ),
+          Hero(
+            tag: document['photoUrl'],
+            child: Image.network(
+              document['photoUrl'],
+              fit: BoxFit.cover,
+              width: double.infinity,
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Text(document['contents']),
+          ),
+        ],
+      ),
+    );
+  }
+
+  Stream<DocumentSnapshot> _followingStream() {
+    return Firestore.instance
+        .collection('following')
+        .document(user.email)
+        .snapshots();
+  }
+
+  // 팔로우
+  void _follow() {
+    Firestore.instance
+        .collection('following')
+        .document(user.email)
+        .setData({document['email']: true});
+
+    Firestore.instance
+        .collection('follower')
+        .document(document['email'])
+        .setData({user.email: true});
+  }
+
+  // 언팔로우
+  void _unfollow() {
+    Firestore.instance
+        .collection('following')
+        .document(user.email)
+        .setData({document['email']: false});
+
+    Firestore.instance
+        .collection('follower')
+        .document(document['email'])
+        .setData({user.email: false});
+  }
+}
