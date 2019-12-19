96a36edaab6ef08214aec7f08d78410f0a79647d
Junsuk Oh <a811219@gmail.com>
2019-12-19 09:59:47 +0900

    챕터 4장 업데이트

--- chapter04/ios/Flutter/flutter_export_environment.sh
+++ chapter04/ios/Flutter/flutter_export_environment.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 # This is a generated file; do not edit or check into version control.
 export "FLUTTER_ROOT=/Users/junsuk/dev/flutter"
-export "FLUTTER_APPLICATION_PATH=/Users/junsuk/dev/FlutterProject/flutter_basic"
+export "FLUTTER_APPLICATION_PATH=/Users/junsuk/dev/tmp/flutter_clone/chapter04"
 export "FLUTTER_TARGET=lib/main.dart"
 export "FLUTTER_BUILD_DIR=build"
 export "SYMROOT=${SOURCE_ROOT}/../build/ios"
